593b4aee411afcef7a5051be51354daa651033a8
Aji Darma S <73057336+socute7@users.noreply.github.com>
2024-10-23 01:00:40 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,16 +16,6 @@ const Footer = () => {
         </a>
         😎
       </p>
-      <p className="pink-text-gradient">
-        No. of Visitors |{" "}
-        <img
-          className="visitcounter"
-          src="https://hitwebcounter.com/counter/counter.php?page=9795911&style=0025&nbdigits=5&type=page&initCount=459"
-          title="Counter Widget"
-          Alt="Visit counter For Websites"
-          border="0"
-        />
-      </p>
     </div>
   );
 };
